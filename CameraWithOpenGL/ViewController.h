e29a0bd1e32676fad05f23711b7bb996e8459759
Bo Chiu (TW ENG) <bochiu@17.media>
2021-09-01 21:11:56 +0800

    Initial Commit

---
+++ CameraWithOpenGL/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CameraWithOpenGL
+//
+//  Created by Bo Chiu on 2021/9/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CameraWithOpenGL/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CameraWithOpenGL
+//
+//  Created by Bo Chiu on 2021/9/1.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
